b4d8704052046f552fbaffd346ce01843ac001a6
phg7495 <135788115+phg7495@users.noreply.github.com>
2023-06-25 22:50:09 +0900

    [Bronze III] Title: 약수 구하기, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,16 @@
+N, K = map(int,input().split())
+
+divisor = []
+
+for i in range(1,int(N**(1/2))+1):
+    if N % i == 0:
+        divisor.append(i)
+        if i**2 != N:
+            divisor.append(N//i)
+
+divisor.sort()
+
+if len(divisor) < K:
+    print(0)
+else:
+    print(divisor[K-1])
