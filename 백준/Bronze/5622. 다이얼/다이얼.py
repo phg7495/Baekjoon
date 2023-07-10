bdd8e679d16497880d0f67a8b32a45e39b7004cf
phg7495 <135788115+phg7495@users.noreply.github.com>
2023-07-10 19:37:25 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,14 +1,10 @@
 word = input()
+alphabet = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"]
 time = 0
 
-for i in word:
-    ascii_word = ord(i)
-    if ascii_word == 83:
-        time += (ascii_word-56)//3 - 1
-    elif ascii_word > 83 and ascii_word < 90:
-        time += (ascii_word-57)//3
-    elif ascii_word == 90:
-        time += (ascii_word-57)//3 - 1
-    else:
-        time +=(ascii_word-56)//3
+for i, alphabet_group in enumerate(alphabet):
+    for j in word:
+        if j in alphabet_group:
+            time += i+3
+
 print(time)
